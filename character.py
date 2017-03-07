4c8adf46f6bfdc0d843f0e0f3aae9f4d38832810
Pedro312 <pedro47.596@gmail.com>
2017-03-07 15:30:47 -0800

    3/7 no armor

--- character.py
+++ character.py
@@ -1,10 +1,11 @@
 class Character(object):
-    def __init__(self, name, hp, damage, attack_speed):
+    def __init__(self, name, hp, damage, attack_speed, armor):
         self.name = name
         self.damage = damage
         self.health = hp
         self.attack_speed = attack_speed
         self.bag = []
+        self.armor = armor
         
     def pick_up(self, item):
         self.bag.append(item)
@@ -22,10 +23,10 @@ class Character(object):
         else:
             print "%s is already dead" % self.name
         
-orc1 = Character('The First Orc', 100, 20, 2)
-orc2 = Character('The Second Orc', 100, 20, 2)
-sam = Character("Sam V", 10, 0.000000000001, 0.0000000001)
-
-#Testing code
-orc1.attack(orc2)
-print "The second orc has %d health." % orc2.health
+orc1 = Character('The First Orc', 100, 20, 2, 0)
+orc2 = Character('The Second Orc', 100, 20, 2, 0)
+sam = Character("Sam V", 10, 0.000000000001, 0.0000000001, 50)
+ed = Character('Edwin Burgos', 9001, 2, 1, 500)
+rob = Character('Roberto Moreno', 80, 100, 2, 200)
+bob = Character('Bobby Vixathep', 1, 20, 2, 0)
+wiebe = Character('Senor Wiebe', 300, 66, 2, 200)
